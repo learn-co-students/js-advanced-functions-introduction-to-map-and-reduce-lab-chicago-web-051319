c2de93452f38d3605a4db3ee4b7a7ef9c40b4c89
Joel Seng <joel29dec@gmail.com>
2019-07-07 13:24:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function mapToNegativize (arr) {
+    return arr.map(e => -e )
+}
+function mapToNoChange (arr) {
+    return arr.map(e => e )
+}
+
+function mapToDouble (arr) {
+    return arr.map(e => e * 2)
+}
+
+function mapToSquare (arr) {
+    return arr.map(e => e ** 2)
+}
+
+function reduceToTotal (arr, start=0) {
+    return arr.reduce((a, c)=>  a + c, start)
+}
+
+function reduceToAllTrue (arr) {
+    return arr.filter(x => !!x);
+}
+
+sourceArray = [1, 2, true, "razmatazz", false]
